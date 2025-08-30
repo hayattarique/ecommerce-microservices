b4f247baad67e969e0a90af03693f0668562c974
hayat <takram403@gmail.com>
2025-08-30 23:04:39 +0530

    eureka-server added

--- auth-service/src/main/java/com/boot/user/AuthServiceApplication.java
+++ auth-service/src/main/java/com/boot/user/AuthServiceApplication.java
@@ -2,6 +2,7 @@ package com.boot.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
 
 @SpringBootApplication
 public class AuthServiceApplication {
